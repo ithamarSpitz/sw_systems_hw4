e7bb0b54b77567b0106cee745840546971427bc0
ithamarSpitz <itas12@gmail.com>
2021-12-31 19:37:18 +0200

    add functions

--- main.c
+++ main.c
@@ -1,9 +1,49 @@
 #include<stdio.h>
 #define INFINITY 9999
-int n;
-char * graph;
+int n; // number of nodes or the number of the biggest node number
+int** graph; // edges of the graph if there is no edge we use 0
 
-int dijkstra(int G[n][n],int n,int startnode, int endnode){
+int** create_graph(int size){
+    int** new_graph = malloc(sizeof(int*)* size);
+    for (int i = 0; i < size; i++){
+        new_graph[i] = malloc(sizeof(int)*size);
+    }
+    for (int i = 0; i < size; i++)
+        for (int j = 0; j < count; j++)
+
+    return new_graph;
+}
+
+int* node_edges(char*, int size){
+
+}
+
+void add_edges_to_graph(int node_number, *int edges){
+    // this function add the node and its edges, the array above it is its edges (were is no edge we use 0)
+    int** new_graph;
+    if(node_number > n){
+        new_graph = create_graph(node_number);
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                new_graph[i][j] = graph[i][j];
+        n = node_number;
+    }
+    // add the edges to the graph 
+    for (int i = 0; i < n; i++){
+        new_graph[node_number][i] = edges[i];
+    }
+    graph = new_graph;
+}
+
+void delete_edges_from_graph(int node_number){
+    // this function puts 0 insteade all the edges of the deleted node
+    for (int i = 0; i < n; i++)
+        graph[node_number][i] = 0;
+    for (int i = 0; i < n; i++)
+        graph[i][node_number] = 0; 
+}
+
+int dijkstra(int startnode, int endnode){
 int cost[n][n],distance[n],pred[n];
 int visited[n],count,mindistance,nextnode,i,j;
 //pred[] stores the predecessor of each node
@@ -11,10 +51,10 @@ int visited[n],count,mindistance,nextnode,i,j;
 //create the cost matrix
 for(i=0;i<n;i++)
 for(j=0;j<n;j++)
-if(G[i][j]==0)
+if(graph[i][j]==0)
 cost[i][j]=INFINITY;
 else
-cost[i][j]=G[i][j];
+cost[i][j]=graph[i][j];
 //initialize pred[],distance[] and visited[]
 for(i=0;i<n;i++){
 distance[i]=cost[startnode][i];
@@ -46,9 +86,11 @@ return distance[endnode];
 } 
 
 int main(){
-scanf("%d",&n);
-int G[n][n],s,d;
 printf("Enter no. of vertices:");
+scanf("%d",&n);
+
+
+int s,d;
 printf("\nEnter the adjacency matrix:\n");
 for(int i=0;i<n;i++)
 for(int j=0;j<n;j++)
