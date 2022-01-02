53251d10e02f42784d747a8a6280a33ed99c4e9c
ithamarSpitz <itas12@gmail.com>
2022-01-03 00:28:04 +0200

    !!!!

--- graf.c
+++ graf.c
@@ -108,51 +108,51 @@ int* g2arr(Graph* graph) {
     return array;
 }
 
-// int dijkstra(Graph* graph, int startnode, int endnode){
-// int n = size_of_array(graph);
-// int* g = g2arr(graph);
-// int cost[n][n],distance[n],pred[n];
-// int visited[n],count,mindistance,nextnode,i,j;
-// //pred[] stores the predecessor of each node
-// //count gives the number of nodes seen so far
-// //create the cost matrix
-// for(i=0;i<n;i++)
-// for(j=0;j<n;j++)
-// if(g[i*n+j]==0)
-// cost[i][j]=INFINITY;
-// else
-// cost[i][j]=g[i*n+j];
-// //initialize pred[],distance[] and visited[]
-// for(i=0;i<n;i++){
-// distance[i]=cost[startnode][i];
-// pred[i]=startnode;
-// visited[i]=0;
-// }
-// distance[startnode]=0;
-// visited[startnode]=1;
-// count=1;
-// while(count<n-1){
-// mindistance=INFINITY;
-// //nextnode gives the node at minimum distance
-// for(i=0;i<n;i++)
-// if(distance[i]<mindistance&&!visited[i]){
-// mindistance=distance[i];
-// nextnode=i;
-// }
-// //check if a better path exists through nextnode
-// visited[nextnode]=1;
-// for(i=0;i<n;i++)
-// if(!visited[i])
-// if(mindistance+cost[nextnode][i]<distance[i]){
-// distance[i]=mindistance+cost[nextnode][i];
-// pred[i]=nextnode;
-// }
-// count++;
-// }
-// return distance[endnode];
-// } 
+int dijkstra(Graph* graph, int startnode, int endnode){
+int n = size_of_array(graph);
+int* g = g2arr(graph);
+int cost[n][n],distance[n],pred[n];
+int visited[n],count,mindistance,nextnode,i,j;
+//pred[] stores the predecessor of each node
+//count gives the number of nodes seen so far
+//create the cost matrix
+for(i=0;i<n;i++)
+for(j=0;j<n;j++)
+if(g[i*n+j]==0)
+cost[i][j]=INFINITY;
+else
+cost[i][j]=g[i*n+j];
+//initialize pred[],distance[] and visited[]
+for(i=0;i<n;i++){
+distance[i]=cost[startnode][i];
+pred[i]=startnode;
+visited[i]=0;
+}
+distance[startnode]=0;
+visited[startnode]=1;
+count=1;
+while(count<n-1){
+mindistance=INFINITY;
+//nextnode gives the node at minimum distance
+for(i=0;i<n;i++)
+if(distance[i]<mindistance&&!visited[i]){
+mindistance=distance[i];
+nextnode=i;
+}
+//check if a better path exists through nextnode
+visited[nextnode]=1;
+for(i=0;i<n;i++)
+if(!visited[i])
+if(mindistance+cost[nextnode][i]<distance[i]){
+distance[i]=mindistance+cost[nextnode][i];
+pred[i]=nextnode;
+}
+count++;
+}
+return distance[endnode];
+} 
 
-// prints receiving Edges of Vertice
+prints receiving Edges of Vertice
 void printReceivingEdges(Graph* graph, char* nameTo) {
     Vertice* curVertice = graph->firstVertice;
     char found = 0;
@@ -554,6 +554,6 @@ int main(){
         printf("%d\n",ar[i]);
     }
     k = dijkstra(g,1,2);
-        printf("%d\n", k);
+    printf("%d\n", k);
     printf("insert works");
 }
