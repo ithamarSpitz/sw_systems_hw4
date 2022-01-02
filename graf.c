ec8107e7baa2810e81918e61a06faa0274a93fcb
ithamarSpitz <itas12@gmail.com>
2022-01-02 22:16:59 +0200

    ;

--- graf.c
+++ graf.c
@@ -520,7 +520,7 @@ char deleteEdgeFromGraph(Graph* graph, char* nameFrom, char* nameTo, unsigned in
 
 int main(){
     Graph* g = initGraph();
-    char* c = malloc(sizeof(char))
+    char* c = malloc(sizeof(char));
     c[0] = '1';
     insertVerticeToGraph(g, c);
     printf("%d", size_of_array(g));
