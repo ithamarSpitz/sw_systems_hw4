3e689130d4a3816fb58c350a9e36d77cdceec8f8
ithamarSpitz <itas12@gmail.com>
2022-01-02 22:49:44 +0200

    ?

--- graf.c
+++ graf.c
@@ -84,70 +84,70 @@ int size_of_array(Graph* graph){
     return counter;
 }
 
-int* g2arr(Graph* graph) {
-    int counter = size_of_array(graph);
+// int* g2arr(Graph* graph) {
+//     int counter = size_of_array(graph);
 
-    int array[counter*counter];
-    for (int i = 0; i < counter; i++){
-        for (int j = 0; j < counter; j++){
-            array[i*counter+j] = 0;
-        }
-    }
+//     int array[counter*counter];
+//     for (int i = 0; i < counter; i++){
+//         for (int j = 0; j < counter; j++){
+//             array[i*counter+j] = 0;
+//         }
+//     }
 
-    Vertice* curVertice = graph->firstVertice;
-    while (curVertice != NULL) {
-        Edge* curEdge = curVertice->firstEdge;
-        while (curEdge != NULL) {
-            array[atoi(curVertice->name)*counter+atoi(curEdge->name)] = curEdge->weight;
-            curEdge = curEdge->nextEdge;
-        }
-        curVertice = curVertice->nextVertice;
-    }
-    return array;
-}
-int dijkstra(Graph* graph, int startnode, int endnode){
-int n = size_of_array(graph);
-int* g = g2arr(graph);
-int cost[n][n],distance[n],pred[n];
-int visited[n],count,mindistance,nextnode,i,j;
-//pred[] stores the predecessor of each node
-//count gives the number of nodes seen so far
-//create the cost matrix
-for(i=0;i<n;i++)
-for(j=0;j<n;j++)
-if(g[i*n+j]==0)
-cost[i][j]=INFINITY;
-else
-cost[i][j]=g[i*n+j];
-//initialize pred[],distance[] and visited[]
-for(i=0;i<n;i++){
-distance[i]=cost[startnode][i];
-pred[i]=startnode;
-visited[i]=0;
-}
-distance[startnode]=0;
-visited[startnode]=1;
-count=1;
-while(count<n-1){
-mindistance=INFINITY;
-//nextnode gives the node at minimum distance
-for(i=0;i<n;i++)
-if(distance[i]<mindistance&&!visited[i]){
-mindistance=distance[i];
-nextnode=i;
-}
-//check if a better path exists through nextnode
-visited[nextnode]=1;
-for(i=0;i<n;i++)
-if(!visited[i])
-if(mindistance+cost[nextnode][i]<distance[i]){
-distance[i]=mindistance+cost[nextnode][i];
-pred[i]=nextnode;
-}
-count++;
-}
-return distance[endnode];
-} 
+//     Vertice* curVertice = graph->firstVertice;
+//     while (curVertice != NULL) {
+//         Edge* curEdge = curVertice->firstEdge;
+//         while (curEdge != NULL) {
+//             array[atoi(curVertice->name)*counter+atoi(curEdge->name)] = curEdge->weight;
+//             curEdge = curEdge->nextEdge;
+//         }
+//         curVertice = curVertice->nextVertice;
+//     }
+//     return array;
+// }
+// int dijkstra(Graph* graph, int startnode, int endnode){
+// int n = size_of_array(graph);
+// int* g = g2arr(graph);
+// int cost[n][n],distance[n],pred[n];
+// int visited[n],count,mindistance,nextnode,i,j;
+// //pred[] stores the predecessor of each node
+// //count gives the number of nodes seen so far
+// //create the cost matrix
+// for(i=0;i<n;i++)
+// for(j=0;j<n;j++)
+// if(g[i*n+j]==0)
+// cost[i][j]=INFINITY;
+// else
+// cost[i][j]=g[i*n+j];
+// //initialize pred[],distance[] and visited[]
+// for(i=0;i<n;i++){
+// distance[i]=cost[startnode][i];
+// pred[i]=startnode;
+// visited[i]=0;
+// }
+// distance[startnode]=0;
+// visited[startnode]=1;
+// count=1;
+// while(count<n-1){
+// mindistance=INFINITY;
+// //nextnode gives the node at minimum distance
+// for(i=0;i<n;i++)
+// if(distance[i]<mindistance&&!visited[i]){
+// mindistance=distance[i];
+// nextnode=i;
+// }
+// //check if a better path exists through nextnode
+// visited[nextnode]=1;
+// for(i=0;i<n;i++)
+// if(!visited[i])
+// if(mindistance+cost[nextnode][i]<distance[i]){
+// distance[i]=mindistance+cost[nextnode][i];
+// pred[i]=nextnode;
+// }
+// count++;
+// }
+// return distance[endnode];
+// } 
 
 // prints receiving Edges of Vertice
 void printReceivingEdges(Graph* graph, char* nameTo) {
