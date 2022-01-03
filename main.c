d5856ea817475bf5d2416ed46e8419f1e5b5d7db
ithamarSpitz <itas12@gmail.com>
2022-01-03 18:00:36 +0200

    now

--- main.c
+++ main.c
@@ -70,7 +70,7 @@ void print_func_arr(char* str, int start){
     printf("\n%c is between %d to %d\n", str[start], start+1, find_next_func(str, start+1));
     int length = find_next_func(str, start+1) - start+1;
     char* dest;
-    realloc(dest, sizeof(*str)*length);
+    dest = realloc(dest, sizeof(*str)*length);
     strncpy(dest, str+start+1, length);
     int* arr = convertStrtoArr(dest);
     printf("length of arr is: %d\n", arr[0]);
