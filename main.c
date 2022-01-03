1a912f77b120ed0c322615eadb4c14d29514f8a2
ithamarSpitz <itas12@gmail.com>
2022-01-03 15:35:42 +0200

    !

--- main.c
+++ main.c
@@ -1,12 +1,37 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include"graf.h"
 
+char *inputString(FILE* fp, size_t size){
+//The size is extended by the input with the value of the provisional
+    char *str;
+    int ch;
+    size_t len = 0;
+    str = realloc(NULL, sizeof(*str)*size);//size is start size
+    if(!str)return str;
+    while(EOF!=(ch=fgetc(fp)) && ch != '\n'){
+        str[len++]=ch;
+        if(len==size){
+            str = realloc(str, sizeof(*str)*(size+=16));
+            if(!str)return str;
+        }
+    }
+    str[len++]='\0';
+
+    return realloc(str, sizeof(*str)*len);
+}
 
 
 
 int main(){
-    Graph* g = initGraph();
-    insertVerticeToGraph(g, '1');
-    printf("%d", size_of_array(g));
+    char *m;
+
+    printf("input string : ");
+    m = inputString(stdin, 10);
+    printf("%s\n", m);
+
+    free(m);
+    return 0;
+    // Graph* g = initGraph();
+    // insertVerticeToGraph(g, '1');
+    // printf("%d", size_of_array(g));
 }
